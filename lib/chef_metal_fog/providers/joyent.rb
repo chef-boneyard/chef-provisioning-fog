8bcdc4576191c2dc151ae89700275016db4e288a
Andrew Bennett <potatosaladx@gmail.com>
2014-07-30 21:42:11 -0600

    Adding initial support for Joyent Public Cloud.

---
+++ lib/chef_metal_fog/providers/joyent.rb
@@ -0,0 +1,55 @@
+require 'fog/joyent'
+
+#   fog:Joyent:<joyent_url>
+module ChefMetalFog
+  module Providers
+    class Joyent < ChefMetalFog::FogDriver
+
+      ChefMetalFog::FogDriver.register_provider_class('Joyent', ChefMetalFog::Providers::Joyent)
+
+      def creator
+        compute_options[:joyent_username]
+      end
+
+      def bootstrap_options_for(action_handler, machine_spec, machine_options)
+        bootstrap_options = symbolize_keys(machine_options[:bootstrap_options] || {})
+
+        bootstrap_options[:tags] = default_tags(machine_spec, bootstrap_options[:tags] || {})
+
+        bootstrap_options[:tags].each do |key, val|
+          bootstrap_options["tag.#{key}"] = val
+        end
+
+        bootstrap_options[:name] ||= machine_spec.name
+
+        bootstrap_options
+      end
+
+      def self.compute_options_for(provider, id, config)
+        new_compute_options = {}
+        new_compute_options[:provider] = provider
+        new_config = { :driver_options => { :compute_options => new_compute_options }}
+        new_defaults = {
+          :driver_options => { :compute_options => {} },
+          :machine_options => { :bootstrap_options => {} }
+        }
+        result = Cheffish::MergedConfig.new(new_config, config, new_defaults)
+
+        new_compute_options[:joyent_url] = id if (id && id != '')
+        credential = Fog.credentials
+
+        new_compute_options[:joyent_username] ||= credential[:joyent_username]
+        new_compute_options[:joyent_password] ||= credential[:joyent_password]
+        new_compute_options[:joyent_keyname] ||= credential[:joyent_keyname]
+        new_compute_options[:joyent_keyfile] ||= credential[:joyent_keyfile]
+        new_compute_options[:joyent_url] ||= credential[:joyent_url]
+        new_compute_options[:joyent_version] ||= credential[:joyent_version]
+
+        id = result[:driver_options][:compute_options][:joyent_url]
+
+        [result, id]
+      end
+
+    end
+  end
+end
