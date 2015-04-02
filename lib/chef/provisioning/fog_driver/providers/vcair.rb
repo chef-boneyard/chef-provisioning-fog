47eb07f03eb9dd2dd738ad6a062be0617e3eb663
hh, taylor, watson <wolfpack+chris+taylor@vulk.co>
2015-04-02 07:48:54 -0500

    refactor of vcair complete.  create and destroy functioning fully

--- lib/chef/provisioning/fog_driver/providers/vcair.rb
+++ lib/chef/provisioning/fog_driver/providers/vcair.rb
@@ -1,8 +1,3 @@
-#require 'fog/vcloud_director'
-
-require 'pry-byebug'
-require 'byebug'
-
 #   fog:Vcair:<client id>
 class Chef
   module Provisioning
@@ -12,13 +7,10 @@ class Chef
           Driver.register_provider_class('Vcair', FogDriver::Providers::Vcair)
 
           def creator
-            Chef::Config[:knife][:vcair_username],
+            Chef::Config[:knife][:vcair_username]
           end
 
           def compute
-            # Vcair
-            #  based on knife vcair add_api_endpoint()
-            # FROM knife vcair plugin -- vcair_service.rb specific connection setup
             @compute ||= begin
                            Chef::Log.debug("vcair_username #{Chef::Config[:knife][:vcair_username]}")
                            Chef::Log.debug("vcair_org #{Chef::Config[:knife][:vcair_org]}")
@@ -45,7 +37,6 @@ class Chef
                          rescue Excon::Errors::Unauthorized => e
                            error_message = "Connection failure, please check your username and password."
                            Chef::Log.error(error_message)
-                           #raise CloudExceptions::ServiceConnectionError, "#{e.message}. #{error_message}"
                            raise "#{e.message}. #{error_message}"
                          rescue Excon::Errors::SocketError => e
                            error_message = "Connection failure, please check your authentication URL."
@@ -58,42 +49,22 @@ class Chef
           def create_many_servers(num_servers, bootstrap_options, parallelizer)
             parallelizer.parallelize(1.upto(num_servers)) do |i|
               clean_bootstrap_options = Marshal.load(Marshal.dump(bootstrap_options)) # Prevent destructive operations on bootstrap_options.
-              # Vcair
-              # FROM knife vcair plugin --  specific server create
               vm=nil
               begin
-                # ORIGINAL driver.rb:
-                #    server = compute.servers.create(bootstrap_options[:server_def])
+                instantiate(clean_bootstrap_options)
 
-                #Chef::Config[:knife][:ssh_password] = ENV['VCAIR_SSH_PASSWORD']
-                #Chef::Config[:ssh_password] = ENV['VCAIR_SSH_PASSWORD']
-                #Chef::Config[:password] = ENV['VCAIR_SSH_PASSWORD']
-                #Chef::Config[:knife][:chef_node_name] = 'centosdemo1'
-                #Chef::Config[:knife][:chef_node_name] = machine_spec.name
-                #Chef::Config[:knife][:chef_node_name] = 'linuxdemoapp1'
-                #Chef::Config[:knife][:chef_node_name] = bootstrap_options[:name]
-
-                # Vcair
-                #  From vcair_server_create.rb
-                i = instantiate(clean_bootstrap_options)
-
-                # vapp(), vm() inlined
                 vapp = vdc.vapps.get_by_name(bootstrap_options[:name])
                 vm = vapp.vms.find {|v| v.vapp_name == bootstrap_options[:name]}
 
-                c = update_customization(clean_bootstrap_options, vm)
+                update_customization(clean_bootstrap_options, vm)
 
-                # TODO: grab cpu from bootstrap
                 if bootstrap_options[:cpus]
                   vm.cpu = bootstrap_options[:cpus]
                 end
                 if bootstrap_options[:memory]
                   vm.memory = bootstrap_options[:memory]
                 end
-                n = update_network(bootstrap_options, vapp, vm)
-
-                # NOTE: called automatically from in start_server
-                #v= vm.power_on
+                update_network(bootstrap_options, vapp, vm)
 
               rescue Excon::Errors::BadRequest => e
                 response = Chef::JSONCompat.from_json(e.response.body)
@@ -109,36 +80,6 @@ class Chef
                 raise e.message
               end
 
-              #vapps.get_by_name(Chef::Config[:knife][:chef_node_name])
-
-              # vms = []
-              # vdc.vapps.all.each do |vapp|
-              #   vms << vapp.vms.all
-              # end
-              # vms = vms.flatten
-
-              #vms = vapp.vms
-              ##server = vms.select{|v| v.vapp_name == machine_spec.name }.first
-              ##server = vms.find{|v| v.vapp_name == Chef::Config[:knife][:chef_node_name]}
-              #server = vms.find{|v| v.vapp_name == bootstrap_options[:name]}
-              ##server = vms.first
-
-              #bootstrap_options[:server_create_timeout]=60
-              # bootstrap_options[:server_create_timeout]=600
-              # bootstrap_options[:create_timeout]=600
-              # bootstrap_options[:start_timeout]=600
-              # machine_options[:server_create_timeout]=600
-              # machine_options[:start_timeout]=600
-              # machine_options[:create_timeout]=600
-              #
-              puts "Waiting for server [wait time = #{bootstrap_options[:create_timeout]}]"
-
-              # wait for it to be ready to do stuff
-              vm.wait_for(Integer(bootstrap_options[:create_timeout])) { puts "."; ready? }
-
-              #puts("\n")
-              #server
-
               yield vm if block_given?
               vm
 
@@ -149,7 +90,6 @@ class Chef
           def start_server(action_handler, machine_spec, server)
 
             # If it is stopping, wait for it to get out of "stopping" transition state before starting
-            # Vcair:
             if server.status == 'stopping'
               action_handler.report_progress "wait for #{machine_spec.name} (#{server.id} on #{driver_url}) to finish stopping ..."
               # Vcair
@@ -161,10 +101,8 @@ class Chef
             # NOTE: Vcair fog does not get server.status via http every time
             server.reload
 
-            # Vcair:
             if server.status == 'off' or server.status != 'on'
               action_handler.perform_action "start machine #{machine_spec.name} (#{server.id} on #{driver_url})" do
-                # Vcair
                 server.power_on
                 machine_spec.location['started_at'] = Time.now.to_i
               end
@@ -196,20 +134,10 @@ class Chef
           end
 
           def ssh_options_for(machine_spec, machine_options, server)
-            # Vcair
-            result = case provider
-                     when 'vclouddirector'
-                       # machine_options = { start_timeout: 600, create_timeout: 600 }.merge(machine_options || {})
-                       # machine_options[:bootstrap_options] = { start_timeout: 600, create_timeout: 600 }.merge(machine_options[:bootstrap_options] || {})
-                       result = { 
-                         auth_methods: [ 'password' ],
-                         timeout: (machine_options[:ssh_timeout] || 600),
-                         password: machine_options[:ssh_password]
-                       }.merge(machine_options[:ssh_options] || {})
-                     else
-                       super(machine_spec, machine_options, server)
-                     end
-            result
+            { auth_methods: [ 'password' ],
+              timeout: (machine_options[:ssh_timeout] || 600),
+              password: machine_options[:ssh_password]
+            }.merge(machine_options[:ssh_options] || {})
           end
 
           def create_ssh_transport(machine_spec, machine_options, server)
@@ -222,13 +150,11 @@ class Chef
 
             remote_host = nil
             # Vcair networking is funky
-            if provider == 'vclouddirector' #driver_url.match(/fog:Vcair/)
-              #if machine_options[:use_private_ip_for_ssh] # Vcair probably needs private ip for now
-              if server.ip_address
-                remote_host = server.ip_address
-              else
-                raise "Server #{server.id} has no private or public IP address!"
-              end
+            #if machine_options[:use_private_ip_for_ssh] # Vcair probably needs private ip for now
+            if server.ip_address
+              remote_host = server.ip_address
+            else
+              raise "Server #{server.id} has no private or public IP address!"
             end
 
             #Enable pty by default
@@ -299,33 +225,12 @@ class Chef
             # TODO: find by catalog item ID and/or NAME
             # TODO: add option to search just public and/or private catalogs
 
-            #Chef::Config[:knife][:image] = 'CentOS64-64BIT'
             #TODO: maybe make a hash for caching
             org.catalogs.map do |cat|
               #cat.catalog_items.get_by_name(config_value(:image))
               cat.catalog_items.get_by_name(bootstrap_options[:image_name])
             end.compact.first
           end
-
-          # def vapp
-          #   #@vapp ||= vdc.vapps.get_by_name(config_value(:chef_node_name))
-          #   vdc.vapps.get_by_name(config_value(:chef_node_name))
-          # end
-
-          # def vm
-          #   @vm ||= vapp.vms.find {|v| v.vapp_name == config_value(:chef_node_name)}
-          # end
-
-          # def network_config
-          #   @network_config ||= vapp.network_config.find do |n|
-          #     n if n[:networkName].match(net.name)
-          #   end
-          # end
-
-          # def config_value(key)
-          #   key = key.to_sym
-          #   Chef::Config[:knife][key] || config[key]
-          # end
 
           def instantiate(bootstrap_options)
             begin
@@ -350,7 +255,6 @@ class Chef
               c.script = open(bootstrap_options[:customization_script]).read
             end
 
-
             # TODO: check machine type and pick accordingly for Chef provisioning
             # password = case config_value(:bootstrap_protocol)
             #            when 'winrm'
@@ -359,7 +263,7 @@ class Chef
             #              config_value(:ssh_password)
             #            end
 
-            password = bootstrap_options[:ssh_password]
+            password = bootstrap_options[:ssh_options][:password]
             if password
               c.admin_password =  password 
               c.admin_password_auto = false
@@ -423,90 +327,13 @@ class Chef
               compute.process_task(nc_task)
           end
 
-          ##### END FROM knife Vcair
-
           def bootstrap_options_for(action_handler, machine_spec, machine_options)
             bootstrap_options = symbolize_keys(machine_options[:bootstrap_options] || {})
-            # if bootstrap_options[:key_path]
-            #   bootstrap_options[:key_name] ||= File.basename(bootstrap_options[:key_path])
-            #   # Verify that the provided key name and path are in line (or create the key pair if not!)
-            #   driver = self
-            #   Provisioning.inline_resource(action_handler) do
-            #     fog_key_pair bootstrap_options[:key_name] do
-            #       private_key_path bootstrap_options[:key_path]
-            #       driver driver
-            #     end
-            #   end
-            # else
-            #   bootstrap_options[:key_name] = overwrite_default_key_willy_nilly(action_handler, machine_spec)
-            # end
-            #
+
             bootstrap_options[:tags]  = default_tags(machine_spec, bootstrap_options[:tags] || {})
             bootstrap_options[:name] ||= machine_spec.name
 
-            ## NOTE: these are displayed on the screen in GREEN
-            #bootstrap_options[:server_create_timeout]=600
-            # bootstrap_options[:create_timeout]=600
-            # bootstrap_options[:start_timeout]=600
-            # bootstrap_options[:ssh_options] = {} if bootstrap_options[:ssh_options].nil?
-            # bootstrap_options[:ssh_options]={timeout: 600}.merge(bootstrap_options[:ssh_options] || {})
-
-            # if machine_options[:vcair_options]
-            #   bootstrap_options[:ssh_options][:password] = machine_options[:vcair_options][:ssh_password] if machine_options[:vcair_options][:ssh_password]
-            #   # bootstrap_options[:ssh_options][:use_private_ip_for_ssh] = machine_options[:vcair_options][:use_private_ip_for_ssh] if machine_options[:vcair_options][:use_private_ip_for_ssh]
-            #   # bootstrap_options[:ssh_options][:auth_methods] = machine_options[:vcair_options][:auth_methods] if machine_options[:vcair_options][:auth_methods]
-            #   bootstrap_options[:image_name] = machine_options[:vcair_options][:bootstrap_options][:image_name]
-            #   bootstrap_options[:memory] = machine_options[:vcair_options][:bootstrap_options][:memory]
-            #   bootstrap_options[:cpus] = machine_options[:vcair_options][:bootstrap_options][:cpus]
-            #   bootstrap_options[:net] = machine_options[:vcair_options][:bootstrap_options][:net]
-            #   bootstrap_options[:customization_script] = machine_options[:vcair_options][:bootstrap_options][:customization_script]
-            # end
-
-
-            # if !bootstrap_options[:image_id]
-            #   if !bootstrap_options[:image_distribution] && !bootstrap_options[:image_name]
-            #     bootstrap_options[:image_distribution] = 'CentOS'
-            #     bootstrap_options[:image_name] = '6.5 x64'
-            #   end
-            #   distributions = compute.images.select { |image| image.distribution == bootstrap_options[:image_distribution] }
-            #   if distributions.empty?
-            #     raise "No images on DigitalOcean with distribution #{bootstrap_options[:image_distribution].inspect}"
-            #   end
-            #   images = distributions.select { |image| image.name == bootstrap_options[:image_name] } if bootstrap_options[:image_name]
-            #   if images.empty?
-            #     raise "No images on DigitalOcean with distribution #{bootstrap_options[:image_distribution].inspect} and name #{bootstrap_options[:image_name].inspect}"
-            #   end
-            #   bootstrap_options[:image_id] = images.first.id
-            # end
-            # if !bootstrap_options[:flavor_id]
-            #   bootstrap_options[:flavor_name] ||= '512MB'
-            #   flavors = compute.flavors.select do |f|
-            #     f.name == bootstrap_options[:flavor_name]
-            #   end
-            #   if flavors.empty?
-            #     raise "Could not find flavor named '#{bootstrap_options[:flavor_name]}' on #{driver_url}"
-            #   end
-            #   bootstrap_options[:flavor_id] = flavors.first.id
-            # end
-            # if !bootstrap_options[:region_id]
-            #   bootstrap_options[:region_name] ||= 'San Francisco 1'
-            #   regions = compute.regions.select { |region| region.name == bootstrap_options[:region_name] }
-            #   if regions.empty?
-            #     raise "Could not find region named '#{bootstrap_options[:region_name]}' on #{driver_url}"
-            #   end
-            #   bootstrap_options[:region_id] = regions.first.id
-            # end
-            # keys = compute.ssh_keys.select { |k| k.name == bootstrap_options[:key_name] }
-            # if keys.empty?
-            #   raise "Could not find key named '#{bootstrap_options[:key_name]}' on #{driver_url}"
-            # end
-            # found_key = keys.first
-            # bootstrap_options[:ssh_key_ids] ||= [ found_key.id ]
-            #
-            # # You don't get to specify name yourself
-            #bootstrap_options[:name] = machine_spec.name
-
-            binding.pry
+            bootstrap_options = bootstrap_options.merge(machine_options.configs[1])
             bootstrap_options
           end
 
@@ -540,8 +367,6 @@ class Chef
             }
             result = Cheffish::MergedConfig.new(new_config, config, new_defaults)
 
-            #new_compute_options[:digitalocean_client_id] = id if (id && id != '')
-            #[result, new_compute_options[:digitalocean_client_id]]
             [result, id]
           end
         end
