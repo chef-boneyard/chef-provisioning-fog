185c35c2cb30962a5b515c6e5dfc319639dd78c3
Christopher F. Auston <causton@softlayer.com>
2016-02-19 16:11:10 -0600

    Fix example machine options

--- examples/softlayer/simple.rb
+++ examples/softlayer/simple.rb
@@ -32,47 +32,47 @@ common_options = {
 
 with_machine_options common_options
 
-machine_batch "SoftLayer Servers" do
-    action todo
+add_machine_options :bootstrap_options => {:flavor_id => 'm1.tiny'} do
+    machine_batch "SoftLayer Servers" do
+        action todo
 
-    add_machine_options :bootstrap_options => {:flavor_id => 'm1.tiny'}
+        machine "centos5" do
+            converge force_converge
+            add_machine_options :bootstrap_options => { :os_code => 'CENTOS_5_64' }
+            role 'all'
+        end
 
-    machine "centos5" do
-        converge force_converge
-        add_machine_options :bootstrap_options => { :os_code => 'CENTOS_5_64' }
-        role 'all'
-    end
+        machine "centos6" do
+            converge force_converge
+            add_machine_options :bootstrap_options => { :os_code => 'CENTOS_6_64' }
+            role 'all'
+        end
 
-    machine "centos6" do
-        converge force_converge
-        add_machine_options :bootstrap_options => { :os_code => 'CENTOS_6_64' }
-        role 'all'
-    end
+        machine "debian7" do
+            converge force_converge
+            add_machine_options :bootstrap_options => { :os_code => 'DEBIAN_7_64' }
+            role 'all'
+        end
 
-    machine "debian7" do
-        converge force_converge
-        add_machine_options :bootstrap_options => { :os_code => 'DEBIAN_7_64' }
-        role 'all'
-    end
+        machine "ubuntu14" do
+            converge force_converge
+            add_machine_options :bootstrap_options => { :os_code => 'UBUNTU_14_64' }
+            role 'all'
+        end
 
-    machine "ubuntu14" do
-        converge force_converge
-        add_machine_options :bootstrap_options => { :os_code => 'UBUNTU_14_64' }
-        role 'all'
-    end
+        machine "centos6.bm" do
+            # I completely override the machine_options here because :flavor_id
+            # will get picked up in deep merge and add options that conflict
+            # with :fixed_configuration_preset.
+            machine_options common_options
 
-    machine "centos6.bm" do
-        # I completely override the machine_options here because :flavor_id
-        # will get picked up in deep merge and add options that conflict
-        # with :fixed_configuration_preset.
-        machine_options common_options
-
-        add_machine_options :bootstrap_options => {
-            :fixed_configuration_preset => 'S1270_8GB_2X1TBSATA_NORAID',
-            :create_timeout => 3600 * 4,
-            :os_code => 'CENTOS_6_64',
-        }
-        converge force_converge
-        role 'all'
+            add_machine_options :bootstrap_options => {
+                :fixed_configuration_preset => 'S1270_8GB_2X1TBSATA_NORAID',
+                :create_timeout => 3600 * 4,
+                :os_code => 'CENTOS_6_64',
+            }
+            converge force_converge
+            role 'all'
+        end
     end
 end
