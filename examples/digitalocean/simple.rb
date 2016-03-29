9c1e4fb5734d2aaf6114ea92f6efff8c0d2e9363
JJ Asghar <jj@chef.io>
2016-03-29 16:04:56 -0500

    Updated per Adams/Robbs suggestions
    - Create status checker
    - Updated example

--- examples/digitalocean/simple.rb
+++ examples/digitalocean/simple.rb
@@ -3,18 +3,15 @@ require "chef/provisioning/fog_driver/recipe_dsl"
 
 with_driver "fog:DigitalOcean"
 
-# It is important that this name be the same on the client and on digitalocean
-# If the key does not exist in one of 'private_key_paths' it will be created
-fog_key_pair "my-simple-key"
-
 add_machine_options :bootstrap_options => {
   :image_distribution => "Ubuntu",
-  :image_name => "12.04.5 x64",
-  :flavor_name => "1GB",
+  :image_name => "14.04.4 x64",
+  :flavor_name => "2GB",
   :region_name => "London 1",
-  :key_name => "my-simple-key"
+  :key_name => "my-simple-key-name-already-on-DO"
 }
 
 machine "mysimpleserver" do
   recipe 'somerecipe'
+  tag 'tagtagtag'
 end
